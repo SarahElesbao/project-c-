706cf59a614181eb4d5d574f2fafac8a4dd7e15c
SarahElesbao <79275710+SarahElesbao@users.noreply.github.com>
2022-12-12 14:15:10 -0300

    commit do projeto

---
+++ Migrations/20221212143918_CreateTable_Produto.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebCRUDMVCSQL.Migrations
+{
+    public partial class CreateTable_Produto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fabricante = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Categoria = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Preço = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebCRUDMVCSQL.Models
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name ="Código")]
+        [Required(ErrorMessage="O nome do usuário é obrigatório")]
+        public int Id { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage="O nome do usuário é obrigatório")]
+        public string Nome { get; set; }
+
+        [Column("Fabricante")]
+        [Display(Name = "Fabricante")]
+        [Required(ErrorMessage="O nome do usuário é obrigatório")]
+        public string Fabricante{ get; set; }
+
+        [Column("Categoria")]
+        [Display(Name = "Categoria")]
+        [Required(ErrorMessage="O nome do usuário é obrigatório")]
+        public string Categoria{ get; set; }
+
+        [Column("Preço")]
+        [Display(Name = "Preço")]
+        [Required(ErrorMessage="O nome do usuário é obrigatório")]
+        public float preco{ get; set; }
+       
+    }
+}
